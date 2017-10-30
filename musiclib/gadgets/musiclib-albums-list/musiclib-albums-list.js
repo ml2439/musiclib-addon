f13610b86181f578c24b720d0530e49605446adf
CL <cl@CLs-MacBook-Pro.local>
2017-10-30 15:07:25 -0400

    first commit

---
+++ musiclib/gadgets/musiclib-albums-list/musiclib-albums-list.js
@@ -0,0 +1,109 @@
+define(function(require, exports, module) {
+    
+        var html = require("text!./musiclib-albums-list.html");
+    
+        var Empty = require("ratchet/dynamic/empty");
+    
+        var UI = require("ui");
+    
+        return UI.registerGadget("musiclib-albums-list", Empty.extend({
+    
+            TEMPLATE: html,
+    
+            /**
+             * Binds this gadget to the /albums route
+             */
+            setup: function() {
+                this.get("/projects/{projectId}/albums", this.index);
+            },
+    
+            /**
+             * Puts variables into the model for rendering within our template.
+             * Once we've finished setting up the model, we must fire callback().
+             *
+             * @param el
+             * @param model
+             * @param callback
+             */
+            prepareModel: function(el, model, callback) {
+    
+                // get the current project
+                var project = this.observable("project").get();
+    
+                // the current branch
+                var branch = this.observable("branch").get();
+    
+                // call into base method and then set up the model
+                this.base(el, model, function() {
+    
+                    // query for musiclib:album instances
+                    branch.queryNodes({ "_type": "musiclib:album" }).then(function() {
+    
+                        // store "albums" on the model (as a list) and then fire callback
+                        model.albums = this.asArray();
+    
+                        // add "imageUrl" attribute to each album
+                        // add "browseUrl" attribute to each album
+                        for (var i = 0; i < model.albums.length; i++)
+                        {
+                            var album = model.albums[i];
+    
+                            album.imageUrl256 = "/preview/repository/" + album.getRepositoryId() + "/branch/" + album.getBranchId() + "/node/" + album.getId() + "/default?size=256&name=preview256&force=true";
+                            album.imageUrl128 = "/preview/repository/" + album.getRepositoryId() + "/branch/" + album.getBranchId() + "/node/" + album.getId() + "/default?size=128&name=preview128&force=true";
+                            album.browseUrl = "/#/projects/" + project._doc + "/documents/" + album._doc;
+                        }
+    
+                        callback();
+                    });
+                });
+            },
+    
+            /**
+             * This method gets called before the rendered DOM element is injected into the page.
+             *
+             * @param el the dom element
+             * @param model the model used to render the template
+             * @param callback
+             */
+            /*
+            beforeSwap: function(el, model, callback)
+            {
+                this.base(el, model, function() {
+                    callback();
+                });
+            },
+            */
+    
+            /**
+             * This method gets called after the rendered DOM element has been injected into the page.
+             *
+             * @param el the new dom element (in page)
+             * @param model the model used to render the template
+             * @param originalContext the dispatch context used to inject
+             * @param callback
+             */
+            afterSwap: function(el, model, originalContext, callback)
+            {
+                this.base(el, model, originalContext, function() {
+    
+                    // find all .media-popups and attach to a lightbox
+                    $(el).find(".media-popup").click(function(e) {
+    
+                        e.preventDefault();
+    
+                        var albumIndex = $(this).attr("data-media-index");
+                        var album = model.albums[albumIndex];
+    
+                        UI.showPopupModal({
+                            "title": "Viewing: " + album.title,
+                            "body": "<div style='text-align:center'><img src='" + album.imageUrl256 + "'></div>"
+                        });
+                    });
+    
+                    callback();
+                });
+            }
+    
+        }));
+    
+    });
---
+++ musiclib/index.js
@@ -0,0 +1,13 @@
+define(function(require) {
+
+    // page: "musiclib-artists-list"
+    require("./gadgets/musiclib-artists-list/musiclib-artists-list.js");
+    // page: "musiclib-albums-list"
+    require("./gadgets/musiclib-albums-list/musiclib-albums-list.js");
+    // page: "musiclib-reviews-list"
+    require("./gadgets/musiclib-reviews-list/musiclib-reviews-list.js");
+    
+    // global CSS overrides
+    require("css!./styles/sample.css");
+
+});
